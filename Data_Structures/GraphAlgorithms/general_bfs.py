fd2e432c597abca5a54ba4609e3b63c3d462fd2f
Harold-Ubuntu_onWindows <hnikoue3@gatech.edu>
2019-11-02 11:39:03 -0700

    Moved  BST To GraphAlgorithms

---
+++ Data_Structures/GraphAlgorithms/general_bfs.py
@@ -0,0 +1,44 @@
+from collections import deque
+class TreeNode:
+    def __init__(self,val):
+        self.val = val
+        self.neighbors = []
+
+def bfs(root: TreeNode):
+    visited = []
+    my_queue = deque()
+    my_queue.append(root)
+    while my_queue:
+        node = my_queue.popLeft()
+        for neighbor in  node.neighbors:
+            if neighbor not in visited:
+                my_queue.append(neighbor)
+                visited.append(neighbor)
+
+
+
+
+def dfs(root: TreeNode):
+    visited = []
+    my_stack = deque()
+    my_stack.append(root)
+    while my_stack:
+        node = my_stack.pop()
+        for neighbor in node.neighbors:
+            if neighbor not in visited:
+                my_stack.append(neighbor)
+                visited.append(neighbor)
+
+
+# Recusive Stack
+def r_dfs(root:TreeNode):
+    visited = []
+
+    def _rdfs(node):
+        if node is None:
+            return
+        for neighbor in node.neighbors:
+            if neighbor not in visited:
+                _rdfs(neighbor)
+                visited.append(neighbor)
+    return _rdfs(root)
