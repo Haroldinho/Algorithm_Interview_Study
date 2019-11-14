5b088264e2a5f393faf0fe3f5fe1541915e4f202
Harry Nikoue <h.a.nikoue@gmail.com>
2019-11-14 15:23:26 -0800

    Added daily coding probem

--- Data_Structures/GraphAlgorithms/daily_coding_problem4.py
+++ Data_Structures/GraphAlgorithms/daily_coding_problem4.py
@@ -3,14 +3,19 @@ Given an array of integers, find the first missing positive integer in linear ti
 In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
 
 For example,  The input [1, 2, 0] should give 3.
+You can modify the array
 """
 
 
 from typing import List
 import unittest
+import random
 
 
 def return_first_missing_positive_num(nums: List) -> int:
+    """
+    This solution is O(nlog n) in time and O(1) in space
+    """
     my_sorted_array = sorted(nums)
     # iterate thrugh array 
     current_num = 1
@@ -43,6 +48,15 @@ class TestTwo(unittest.TestCase):
 
     def test_value(self): 
         self.assertEqual(self._result, 3)
+
+def main():
+    """
+     Big test create an array remove an element and shuffle the array
+    """
+    n = 200
+    rand_idx = random.randint(0,199)
+    my_array = [i for i in range(n)]
+    del my_array[rand_idx]
 
 if __name__ == "__main__":
     unittest.main()
